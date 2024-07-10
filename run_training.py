ac1123094444681986346a0881ed444537512422
Mattia018 <mattiapresta15@gmail.com>
2024-07-10 16:35:13 +0200

    cuda

--- run_training.py
+++ run_training.py
@@ -1,5 +1,8 @@
 import sys
 from lib.training.execute import get_configs_from_args, execute
+import os
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0' 
 
 if __name__ == '__main__':
     config = get_configs_from_args(sys.argv)
